7eca0eeaa0eb5eb01bf9c0311d7158a8887decb9
atreyeeghorai <ghoraiatreyee@gmail.com>
2026-05-03 17:47:08 +0530

    fix: Update project link in projects.js to point to the correct portfolio site

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,6 @@ export const projects = [
     title:"Devatreyee",
     description: "🚀👨‍💻❤️Devatreyee is a personal portfolio website showcasing my projects, skills, and experiences in web development. It features a clean design, responsive layout, and easy navigation to highlight my work effectively. 🚀",
     image:"https://res.cloudinary.com/dyb8aqa60/image/upload/v1777810380/Screenshot_2026-05-03_at_5.39.51_PM_kxxtqy.png ",
-    link: "https://devsumangal.netlify.app/",
+    link: "https://devatreyee.netlify.app/",
   }
 ];
